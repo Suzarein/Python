d6ad652df9f43a895f25002457a7fa01e1c91962
Suzarein <76208350+Suzarein@users.noreply.github.com>
2020-12-19 09:46:43 +0800

    Add files via upload

---
+++ PortScan/PortScanner.py
@@ -0,0 +1,54 @@
+import socket
+from IPy import IP
+
+def DefaultNumber(Port):
+    if Port == '':
+        Port = 100
+        return Port
+    else:
+        Port = int(Port)
+        return Port
+
+def MultiIP(IPAddress, Port):
+    print('\nScanning ' + str(IPAddress) + ' :')
+    Scan(IPv4(IPAddress), Port)
+    print('IP Address of ' + str(IPAddress) + ' is: ' + IPv4(IPAddress))
+
+def IPv4(IPro):
+    try:
+        IP(IPro)
+        return IPro
+    except ValueError:
+        return socket.gethostbyname(IPro)
+
+def Seperator(IPs):
+    if ',' in IPAddress:
+        for IPplus in IPAddress.split(','):
+            MultiIP(IPplus.strip(' '), Port)
+    else:
+        Scan(IPAddress, Port)
+
+def Scan(IPAddress, Port):
+    C = 0
+    O = 0
+    for Port in range(1, Port+1):
+        try:
+            sock = socket.socket()
+            sock.settimeout(.5)
+            sock.connect((IPAddress, Port))
+            print('Port ' + str(Port) + ' is Open')
+            O+= 1
+        except:
+            print('Port ' + str(Port) + ' is Closed')
+            C+= 1
+
+    print('Number of Closed Ports: ' + str(O))
+
+try:
+    IPAddress = input('Enter Website Name/IP Address: ')
+    Port = input('Enter Number of Ports (Default is 100: )')
+    Port = DefaultNumber(Port)
+    Seperator(IPAddress)
+except ValueError:
+    print('Invalid Entry')
+
---
+++ PortScan/README.md
@@ -0,0 +1,4 @@
+# Python
+Usage: First, Input the Website Name or IP Address with a comma seperator. 
+
+Second, Type the Number of Ports you want to Probe.
